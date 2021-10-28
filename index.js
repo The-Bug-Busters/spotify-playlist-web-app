215ef7c932a5b7b7da850c3791d6fbd4994b0c94
Julian Yaman <julianyaman@posteo.eu>
2021-10-28 21:22:25 +0200

    Abkacking Part 3

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ app.get("/", (req, res, next) => {
     res.json({'message': 'Welcome!'});
 });
 
-app.listen(80, () => {
-    console.log("Server running on port 80");
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
 });
